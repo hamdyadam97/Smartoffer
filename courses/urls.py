1f2b9c1ea84d2cdbff61c848e582e15f38c6ee33
hamdyadam97 <hamdyadam97@gmail.com>
2026-05-22 16:37:55 +0300

    cource

--- courses/urls.py
+++ courses/urls.py
@@ -6,14 +6,14 @@ from .views import (
 
 urlpatterns = [
     path('masters/', MasterListView.as_view(), name='master-list'),
-    path('masters/<str:slug>/', MasterDetailView.as_view(), name='master-detail'),
     path('masters/create/', MasterCreateView.as_view(), name='master-create'),
+    path('masters/<str:slug>/', MasterDetailView.as_view(), name='master-detail'),
     path('masters/<str:slug>/update/', MasterUpdateView.as_view(), name='master-update'),
     path('masters/<str:slug>/delete/', MasterDeleteView.as_view(), name='master-delete'),
 
     path('courses/', CourseListView.as_view(), name='course-list'),
-    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
     path('courses/create/', CourseCreateView.as_view(), name='course-create'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
     path('courses/<int:pk>/update/', CourseUpdateView.as_view(), name='course-update'),
     path('courses/<int:pk>/delete/', CourseDeleteView.as_view(), name='course-delete'),
 ]
