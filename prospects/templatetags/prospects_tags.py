8874f8e9006e1f935dabb9dd7d001671c59dbc3d
Hamdy <hamdyadam97@gmail.com>
2026-07-16 10:59:28 +0300

    upload

--- prospects/templatetags/prospects_tags.py
+++ prospects/templatetags/prospects_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..utils import get_user_root_branch_ids
+from ..utils import get_user_root_branch_ids, is_root_branch
 
 register = template.Library()
 
@@ -10,3 +10,15 @@ def has_root_prospect_perm(user):
     if not user or not user.is_authenticated:
         return False
     return bool(get_user_root_branch_ids(user, 'view_prospect'))
+
+
+@register.filter
+def has_non_root_student_perm(user):
+    """Return True if user can view students on any non-Root branch."""
+    if not user or not user.is_authenticated:
+        return False
+    if user.is_executive():
+        from core.models import Branch
+        return Branch.objects.exclude(company__name__icontains='جذور').exists()
+    branches = user.get_branches_for_perm('view_student')
+    return any(not is_root_branch(b) for b in branches)
