c112ff5e57392bad42376a8bbe3161475fe878cf
hamdyadam97 <hamdyadam97@gmail.com>
2026-07-23 10:37:26 +0300

    upload

--- smartoffer_django/settings.py
+++ smartoffer_django/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production')
 
-DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
